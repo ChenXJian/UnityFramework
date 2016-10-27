7c8562036270c529877d265858efa2db2b728d08
meta42 <648398613@qq.com>
2016-10-27 22:11:22 +0800

    cubemap

---
+++ UnityFramework/UnityProject/Assets/Framework/Editor/GenerateCubemap.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class GenerateCubemap : ScriptableWizard
+{
+
+
+    public Transform renderPos;
+    public Cubemap cubemap;
+
+
+
+    void OnWizardUpdate()
+    {
+        helpString = "指定渲染位置与Cubemap资源";
+
+        if (renderPos != null && cubemap != null)
+        {
+            isValid = true;
+        }
+        else
+        {
+            isValid = false;
+        }
+    }
+
+
+    void OnWizardCreate()
+    {
+        GameObject go = new GameObject("camera", typeof(Camera));
+
+        go.transform.position = renderPos.position;
+        go.transform.rotation = renderPos.rotation;
+
+        go.GetComponent<Camera>().RenderToCubemap(cubemap);
+        DestroyImmediate(go);
+    }
+
+    [MenuItem("Tool/GenerateCubemap")]
+    static void RenderCubemap()
+    {
+        ScriptableWizard.DisplayWizard("RenderCubemap", typeof(GenerateCubemap), "Render");
+    }
+}
